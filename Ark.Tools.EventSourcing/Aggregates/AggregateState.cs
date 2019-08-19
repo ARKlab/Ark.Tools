9f60f7f70111142a334571bf96501f78533e8adc
Andrea Cuneo <andrea.cuneo@ark-energy.eu>
2019-08-19 16:52:03 +0200

    [EventSourcing] Make sure AggregateState internal properties are deserialized correctly marking them explicitly as DataMember

--- Ark.Tools.EventSourcing/Aggregates/AggregateState.cs
+++ Ark.Tools.EventSourcing/Aggregates/AggregateState.cs
@@ -1,13 +1,15 @@
-﻿namespace Ark.Tools.EventSourcing.Aggregates
+﻿using System.Runtime.Serialization;
+
+namespace Ark.Tools.EventSourcing.Aggregates
 {
+    [DataContract]
 	public abstract class AggregateState<TAggregateState, TAggregate> : IAggregateState
         where TAggregateState : AggregateState<TAggregateState, TAggregate>, new()
         where TAggregate : IAggregate
     {
+        [DataMember]
         public string Identifier { get; internal set; }
+        [DataMember]
         public long Version { get; internal set; }
 	}
-
-
-
 }
