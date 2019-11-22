41736cef96c072b10d3f9536152bf8720f0e4bcc
Marco Giannotti <marco.giannnotti@ark-energy.eu>
2019-11-22 09:32:08 +0100

    Program aligned for AspNet core  3.0

--- WebApplicationDemo/Program.cs
+++ WebApplicationDemo/Program.cs
@@ -2,10 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
+using Ark.Tools.Nodatime;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,75 +16,111 @@ namespace WebApplicationDemo
 {
 	public static class Program
 	{
-		//public static IHostBuilder GetHostBuilder(string[] args)
-		//{
-		//	args = args ?? Array.Empty<string>();
+		public static IHostBuilder GetHostBuilder(string[] args)
+		{
+			args = args ?? Array.Empty<string>();
 
-		//	var host = Host.CreateDefaultBuilder(args).Config(args);
+			var host = Host.CreateDefaultBuilder(args).Config(args);
 
-		//	return host;
-		//}
+			return host;
+		}
 
-		//public static IHostBuilder Config(this IHostBuilder builder, string[] args)
-		//{
-		//	return builder
-		//		//.UseContentRoot(Directory.GetCurrentDirectory())
-		//		.ConfigureWebHostDefaults(webBuilder =>
-		//		{
-		//			webBuilder.UseKestrel(o =>
-		//			{
-		//				//o.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10);
-		//			})
-		//			.UseIISIntegration()
-		//			.UseStartup<Startup>();
+		public static IHostBuilder Config(this IHostBuilder builder, string[] args)
+		{
+			return builder
+				//.UseContentRoot(Directory.GetCurrentDirectory())
+				.ConfigureWebHostDefaults(webBuilder =>
+				{
+					webBuilder.ConfigureKestrel(serverOptions =>
+					{
+						// Set properties and call methods on options
+					})
+					.CaptureStartupErrors(true)
+					.UseSetting(WebHostDefaults.DetailedErrorsKey, "true")
+					.UseSetting(WebHostDefaults.PreventHostingStartupKey, "true")
+					.ConfigureAppConfiguration((hostingContext, config) =>
+					{
+						var env = hostingContext.HostingEnvironment;
 
-		//			//.UseSetting(HostDefaults.DetailedErrorsKey, "true")
-		//			//.UseSetting(HostDefaults.PreventHostingStartupKey, "true")
-		//		});
-		//}
+						config
+							.SetBasePath(Directory.GetCurrentDirectory())
+							.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+							.AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+							.AddEnvironmentVariables()
+							.AddApplicationInsightsSettings(developerMode: env.IsDevelopment())
+							.AddCommandLine(args)
+							;
+					})
+					.ConfigureLogging((hostingContext, logging) =>
+					{
+						logging.AddConfiguration(hostingContext.Configuration.GetSection("Logging"));
+						logging.AddConsole();
+						logging.AddDebug();
+					})
+					.UseStartup<Startup>();
+				});
+		}
 
-		//public static void InitStatic(string[] args)
-		//{
-		//	args = args ?? Array.Empty<string>();
-		//}
+		public static void InitStatic(string[] args)
+		{
+			args = args ?? Array.Empty<string>();
 
-		//public static async Task Main(string[] args)
-		//{
-		//	try
-		//	{
-		//		InitStatic(args);
+			//DapperNodaTimeSetup.Register();
+			NodeTimeConverter.Register();
+			ServicePointManager.UseNagleAlgorithm = true;
+			ServicePointManager.Expect100Continue = false;
+			ServicePointManager.CheckCertificateRevocationList = true;
+			ServicePointManager.DefaultConnectionLimit = 250;
+			ServicePointManager.EnableDnsRoundRobin = true;
+			ServicePointManager.DnsRefreshTimeout = 4 * 60 * 1000;
 
-		//		using (var h = GetHostBuilder(args)
-		//			.Build())
-		//		{
-		//			await h.RunAsync();
-		//		}
-		//	}
-		//	catch (Exception ex)
-		//	{
-		//		NLog.LogManager.GetLogger("Main").Fatal(ex, $@"Unhandled Fatal Exception occurred: {ex.Message}");
-		//		Console.WriteLine(ex.ToString());
-		//	}
-		//	finally
-		//	{
-		//		NLog.LogManager.GetLogger("Main").Info("Shutting down");
-		//		NLog.LogManager.Flush();
-		//	}
-		//}
+			var cfg = new ConfigurationBuilder()
+				.SetBasePath(Directory.GetCurrentDirectory())
+				.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+				.AddJsonFile($"appsettings.{Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT")}.json", optional: true)
+				.AddEnvironmentVariables()
+				.AddCommandLine(args)
+				.Build()
+				;
+		}
+
+		public static async Task Main(string[] args)
+		{
+			try
+			{
+				InitStatic(args);
+
+				using (var h = GetHostBuilder(args)
+					.Build())
+				{
+					await h.RunAsync();
+				}
+			}
+			catch (Exception ex)
+			{
+				NLog.LogManager.GetLogger("Main").Fatal(ex, $@"Unhandled Fatal Exception occurred: {ex.Message}");
+				Console.WriteLine(ex.ToString());
+			}
+			finally
+			{
+				NLog.LogManager.GetLogger("Main").Info("Shutting down");
+				NLog.LogManager.Flush();
+			}
+		}
 
 
 		//***************** DEFAULT *******************/
-		
-		public static void Main(string[] args)
-		{
-			CreateHostBuilder(args).Build().Run();
-		}
 
-		public static IHostBuilder CreateHostBuilder(string[] args) =>
-			Host.CreateDefaultBuilder(args)
-				.ConfigureWebHostDefaults(webBuilder =>
-				{
-					webBuilder.UseStartup<Startup>();
-				});
+		//public static void Main(string[] args)
+		//{
+		//	CreateHostBuilder(args).Build().Run();
+		//}
+
+		//public static IHostBuilder CreateHostBuilder(string[] args) =>
+		//	Host.CreateDefaultBuilder(args)
+		//		.ConfigureWebHostDefaults(webBuilder =>
+		//		{
+		//			webBuilder.UseStartup<Startup>();
+		//		});
 	}
 }
