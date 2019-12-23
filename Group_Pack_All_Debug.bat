8edb0d5c7007f7d1f04194bf75bf38ea29219256
Marco Giannotti <marco.giannnotti@ark-energy.eu>
2019-12-23 16:03:57 +0100

    Published beta01 of version 2.0.0

---
+++ Group_Pack_All_Debug.bat
@@ -0,0 +1,13 @@
+@echo off
+
+call Group_Pack_Core_Debug.bat
+call Group_Pack_FTPClient_Debug.bat
+call Group_Pack_ResourceWatcher_Debug.bat
+call Group_Pack_EntityFrameworkCore_Debug.bat
+call Group_Pack_RavenDb_Debug.bat
+call Group_Pack_AspnetCore_Debug.bat
+call Group_Pack_EventSourcing_Debug.bat
+
+pause
+
+:end
---
+++ Group_Pack_AspnetCore_Debug.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal
+set PROGRAMS=%ProgramFiles(x86)%
+for %%e in (Community Professional Enterprise) do (
+    if exist "%PROGRAMS%\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild.exe" (
+        set "MSBUILD=%PROGRAMS%\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild.exe"
+    )
+)
+if exist "%MSBUILD%" goto :operations
+
+:nomsbuild
+echo Microsoft Build version 15.1 (or later) does not appear to be
+echo installed on this machine, which is required to build the solution.
+goto end
+
+:operations
+setlocal
+
+"%MSBUILD%" "Ark.Tools.AspNetCore\Ark.Tools.AspNetCore.csproj"																			/t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.ApplicationInsights\Ark.Tools.AspNetCore.ApplicationInsights.csproj"									/t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.Auth0\Ark.Tools.AspNetCore.Auth0.csproj"																/t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.BasicAuthAuth0Proxy\Ark.Tools.AspNetCore.BasicAuthAuth0Proxy.csproj"									/t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.BasicAuthAzureActiveDirectoryProxy\Ark.Tools.AspNetCore.BasicAuthAzureActiveDirectoryProxy.csproj"	/t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.CommaSeparatedParameters\Ark.Tools.AspNetCore.CommaSeparatedParameters.csproj"						/t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.MessagePack\Ark.Tools.AspNetCore.MessagePack.csproj"												    /t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.NestedStartup\Ark.Tools.AspNetCore.NestedStartup.csproj"											    /t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.RavenDb\Ark.Tools.AspNetCore.RavenDb.csproj"															/t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.Swashbuckle\Ark.Tools.AspNetCore.Swashbuckle.csproj"												    /t:Pack /p:Configuration=Debug
+"%MSBUILD%" "Ark.Tools.AspNetCore.XlsxOutputFormatter\Ark.Tools.AspNetCore.XlsxOutputFormatter.csproj"								    /t:Pack /p:Configuration=Debug
+
+pause
+
+:end
