c82a838d030243702c654434d5ffd1805f9d0a91
Marco Giannotti <marco.giannnotti@ark-energy.eu>
2020-01-22 11:29:49 +0100

    Added JwtTokenBuilder for SpecFlow tests

---
+++ Ark.Tools.SpecFlow/Auth/JwtTokenBuilder.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+
+namespace Ark.Tools.SpecFlow.Auth
+{
+    public sealed class JwtTokenBuilder
+    {
+        private SecurityKey securityKey = null;
+        private string subject = "";
+        private string issuer = "";
+        private string audience = "";
+        private List<Claim> claims = new List<Claim>();
+        private int expiryInMinutes = 5;
+
+        public JwtTokenBuilder AddSecurityKey(SecurityKey securityKey)
+        {
+            this.securityKey = securityKey;
+            return this;
+        }
+
+        public JwtTokenBuilder AddSubject(string subject)
+        {
+            this.subject = subject;
+            return this;
+        }
+
+        public JwtTokenBuilder AddIssuer(string issuer)
+        {
+            this.issuer = issuer;
+            return this;
+        }
+
+        public JwtTokenBuilder AddAudience(string audience)
+        {
+            this.audience = audience;
+            return this;
+        }
+
+        public JwtTokenBuilder AddClaim(string type, string value)
+        {
+            this.claims.Add(new Claim(type, value));
+            return this;
+        }
+
+        public JwtTokenBuilder RemoveClaim(string type)
+        {
+            this.claims.RemoveAll(x => x.Type == type);
+            return this;
+        }
+
+        public JwtTokenBuilder ClearClaims()
+        {
+            this.claims.Clear();
+            return this;
+        }
+
+        public JwtTokenBuilder AddExpiry(int expiryInMinutes)
+        {
+            this.expiryInMinutes = expiryInMinutes;
+            return this;
+        }
+
+        public JwtToken Build()
+        {
+            EnsureArguments();
+
+            var claims = new List<Claim>
+            {
+              new Claim(JwtRegisteredClaimNames.Sub, this.subject),
+              new Claim(JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString())
+            }
+            .Union(this.claims);
+
+            var token = new JwtSecurityToken(
+                              issuer: this.issuer,
+                              audience: this.audience,
+                              claims: claims,
+                              expires: DateTime.UtcNow.AddMinutes(expiryInMinutes),
+                              signingCredentials: new SigningCredentials(
+                                                        this.securityKey,
+                                                        SecurityAlgorithms.HmacSha256));
+
+            return new JwtToken(token);
+        }
+
+        #region " private "
+
+        private void EnsureArguments()
+        {
+            if (this.securityKey == null)
+                throw new ArgumentNullException("Security Key");
+
+            if (string.IsNullOrEmpty(this.subject))
+                throw new ArgumentNullException("Subject");
+
+            if (string.IsNullOrEmpty(this.issuer))
+                throw new ArgumentNullException("Issuer");
+
+            if (string.IsNullOrEmpty(this.audience))
+                throw new ArgumentNullException("Audience");
+        }
+
+        #endregion
+    }
+}
