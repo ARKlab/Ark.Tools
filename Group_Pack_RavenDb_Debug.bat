7f20abc98731a0eba12967aa0c2176091677ad9a
Michael Geraghty <michael.geraghty@ark-energy.eu>
2022-02-08 16:05:24 +0000

    update vs version

--- Group_Pack_RavenDb_Debug.bat
+++ Group_Pack_RavenDb_Debug.bat
@@ -2,14 +2,14 @@
 setlocal
 set PROGRAMS=%ProgramFiles(x86)%
 for %%e in (Community Professional Enterprise) do (
-    if exist "%PROGRAMS%\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild.exe" (
-        set "MSBUILD=%PROGRAMS%\Microsoft Visual Studio\2019\%%e\MSBuild\Current\Bin\MSBuild.exe"
+    if exist "%PROGRAMS%\Microsoft Visual Studio\2022\%%e\MSBuild\Current\Bin\MSBuild.exe" (
+        set "MSBUILD=%PROGRAMS%\Microsoft Visual Studio\2022\%%e\MSBuild\Current\Bin\MSBuild.exe"
     )
 )
 if exist "%MSBUILD%" goto :operations
 
 :nomsbuild
-echo Microsoft Build version 15.1 (or later) does not appear to be
+echo Microsoft Build version 17.0 (or later) does not appear to be
 echo installed on this machine, which is required to build the solution.
 goto end
 
