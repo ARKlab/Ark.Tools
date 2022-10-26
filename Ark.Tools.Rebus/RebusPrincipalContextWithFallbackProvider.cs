3d95fc7616f77b1a71b33a0cc4efd1bc6ca4b8a4
Alessandro Belleno <alessandro.belleno@ark-energy.eu>
2022-10-26 16:24:48 +0200

    added some helper classes for IContextProvider<ClaimPricipal>

---
+++ Ark.Tools.Rebus/RebusPrincipalContextWithFallbackProvider.cs
@@ -0,0 +1,22 @@
+﻿using Ark.Tools.Solid;
+using System.Security.Claims;
+
+
+namespace Ark.Tools.Rebus
+{
+    public class RebusPrincipalContextWithFallbackProvider : IContextProvider<ClaimsPrincipal>
+    {
+        private readonly IMessageContextProvider _messageContextProvider;
+        private readonly ClaimsPrincipal _fallback;
+
+        public RebusPrincipalContextWithFallbackProvider(IMessageContextProvider messageContextProvider)
+        {
+            _messageContextProvider = messageContextProvider;
+            _fallback = new ClaimsPrincipal(new ClaimsIdentity(new[]{
+                new Claim(ClaimTypes.NameIdentifier, "SYSTEM")
+                }, "SYSTEM"));
+        }
+
+        public ClaimsPrincipal Current => _messageContextProvider.Current?.IncomingStepContext.Load<ClaimsPrincipal>() ?? _fallback;
+    }
+}
---
+++ Ark.Tools.Solid.Authorization/FixedPrincipalContextProvider.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Ark.Tools.Authorization;
+using System.Threading;
+using SimpleInjector;
+using System.Security.Claims;
+
+namespace Ark.Tools.Solid.Authorization
+{
+    public class FixedPrincipalContextProvider : IContextProvider<ClaimsPrincipal>
+    {
+        private readonly Func<ClaimsPrincipal> _getter;
+
+        public FixedPrincipalContextProvider(Func<ClaimsPrincipal> getter)
+        {
+            _getter = getter;
+        }
+
+        public ClaimsPrincipal Current => _getter();
+    }
+}
