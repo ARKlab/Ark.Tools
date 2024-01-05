9cd3d11d2354e149ceec0c7c89b31d88dfe056a2
PeterRimmer <peter.rimmer@ark-energy.eu>
2024-01-05 08:29:54 +0000

    Backout changes to core
    #16206

--- Ark.Tools.FtpClient.Core/IFtpClientPoolFactory.cs
+++ Ark.Tools.FtpClient.Core/IFtpClientPoolFactory.cs
@@ -3,7 +3,6 @@
 using System;
 using System.Net;
 using System.Text;
-using ArxOne.Ftp;
 
 namespace Ark.Tools.FtpClient.Core
 {
--- Ark.Tools.ResourceWatcher.WorkerHost.Ftp/IFtpConfig.cs
+++ Ark.Tools.ResourceWatcher.WorkerHost.Ftp/IFtpConfig.cs
@@ -2,11 +2,8 @@
 // Licensed under the MIT License. See LICENSE file for license information. 
 using Ark.Tools.FtpClient.Core;
 
-using ArxOne.Ftp;
-
 using System;
 using System.Net;
-using System.Text;
 
 namespace Ark.Tools.ResourceWatcher.WorkerHost.Ftp
 {
