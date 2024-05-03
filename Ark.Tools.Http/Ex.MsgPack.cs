a4e1399dfea90dce1763cc1ef1d578a92d421a2a
Andrea Cuneo <andrea.cuneo@ark-energy.eu>
2024-05-03 19:06:56 +0000

    chore: update copyrights

--- Ark.Tools.Http/Ex.MsgPack.cs
+++ Ark.Tools.Http/Ex.MsgPack.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2023 Ark Energy S.r.l. All rights reserved.
+﻿// Copyright (C) 2024 Ark Energy S.r.l. All rights reserved.
 // Licensed under the MIT License. See LICENSE file for license information. 
 using Flurl;
 using Flurl.Http;
