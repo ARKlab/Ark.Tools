5250359a10b8a73269602e3890caae45f6597855
Andrea Cuneo <andrea.cuneo@ark-energy.eu>
2024-08-13 17:49:38 +0200

    improv: use MailAddress instead of a complex Regex for validation

--- Ark.Tools.Core/EmailValidator.cs
+++ Ark.Tools.Core/EmailValidator.cs
@@ -1,34 +1,44 @@
 ﻿// Copyright (C) 2024 Ark Energy S.r.l. All rights reserved.
-// Licensed under the MIT License. See LICENSE file for license information. 
-using System.Globalization;
+// Licensed under the MIT License. See LICENSE file for license information.
+using System.Net.Mail;
+using System;
 using System.Text.RegularExpressions;
 
 namespace Ark.Tools.Core
 {
-    public static class EmailValidator
+    public static partial class EmailValidator
     {
+        [Obsolete("IsValid() now uses MailAddress.TryCreate which is more performant. This property is going to be removed in next major")]
         public static Regex Regex { get; } = _validEmailAddressRegEx();
 
         public static bool IsValid(string emailAddress)
         {
-            return Regex.IsMatch(emailAddress);
+#if NET6_0_OR_GREATER
+            return MailAddress.TryCreate(emailAddress, out var _);
+#else
+            try
+            {
+                var _ = new MailAddress(emailAddress);
+                return true;
+            }
+            catch (FormatException)
+            {
+                return false;
+            }
+#endif
         }
 
+        private const string _regexRFC5322 = @"^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|""(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*"")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$";
+
+#if NET8_0_OR_GREATER
+        [GeneratedRegex(_regexRFC5322, RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture)]
+        private static partial Regex _validEmailAddressRegEx();
+#else
         private static Regex _validEmailAddressRegEx()
         {
-            string qtext = "[^\\x0d\\x22\\x5c\\x80-\\xff]"; // <any CHAR excepting <">, "\" & CR, and including linear-white-space>
-            string dtext = "[^\\x0d\\x5b-\\x5d\\x80-\\xff]"; // <any CHAR excluding "[", "]", "\" & CR, & including linear-white-space>
-            string atom = "[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+"; // *<any CHAR except specials, SPACE and CTLs>
-            string quoted_pair = "\\x5c[\\x00-\\x7f]"; // "\" CHAR 
-            string quoted_string = $"\\x22({qtext}|{quoted_pair})*\\x22"; // <"> *(qtext/quoted-pair) <">
-            string word = $"({atom}|{quoted_string})"; //atom / quoted-string
-            string domain_literal = $"\\x5b({dtext}|{quoted_pair})*\\x5d"; // "[" *(dtext / quoted-pair) "]"
-            string domain_ref = atom; // atom 
-            string sub_domain = string.Format(CultureInfo.InvariantCulture, "({0}|{1})", domain_ref, domain_literal); // domain-ref / domain-literal
-            string domain = string.Format(CultureInfo.InvariantCulture, "{0}(\\x2e{0})*", sub_domain); // sub-domain *("." sub-domain)
-            string local_part = string.Format(CultureInfo.InvariantCulture, "{0}(\\x2e{0})*", word); // word *("." word) 
-            string addr_spec = string.Format(CultureInfo.InvariantCulture, "{0}\\x40{1}", local_part, domain); //local-part "@" domain
-            return new Regex(string.Format(CultureInfo.InvariantCulture, "^{0}$", addr_spec), RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture);
+            return new Regex(_regexRFC5322, RegexOptions.Compiled | RegexOptions.CultureInvariant | RegexOptions.ExplicitCapture);
         }
+#endif
+
     }
 }
